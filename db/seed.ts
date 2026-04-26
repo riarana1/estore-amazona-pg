03ff2d0634150513c0462b130b6772e0a10d6369
riarana1 <riarana1@yahoo.com>
2026-04-26 12:24:27 -0500

    06. setup drizzle orm and postgres database

---
+++ db/seed.ts
@@ -0,0 +1,35 @@
+import { cwd } from "node:process"
+import nextEnv from "@next/env"
+
+import { drizzle } from "drizzle-orm/node-postgres"
+import { Client } from "pg"
+
+import * as schema from "./schema"
+import sampleData from "@/lib/sample-data"
+
+const { loadEnvConfig } = nextEnv
+loadEnvConfig(cwd())
+
+const main = async () => {
+  try {
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    })
+    await client.connect()
+    const db = drizzle(client)
+
+    await db.delete(schema.products)
+
+    const resProducts = await db
+      .insert(schema.products)
+      .values(sampleData.products)
+      .returning()
+    console.log({ resProducts })
+    await client.end()
+  } catch (error) {
+    console.error(error)
+    throw new Error("Failed to seed database", { cause: error })
+  }
+}
+
+main()

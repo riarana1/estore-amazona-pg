9e202394d02577f5ad79f78109afeb9d24038609
riarana1 <riarana1@yahoo.com>
2026-04-26 10:51:43 -0500

    04. create website layout

---
+++ components/shared/footer.tsx
@@ -0,0 +1,13 @@
+import { APP_NAME } from "@/lib/constants"
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center p-5">
+        2026 {APP_NAME}. All Rights reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
